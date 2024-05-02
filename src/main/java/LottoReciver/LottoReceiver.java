d24685b2801a5476949eb3ec5c192524bdb80436
Oskar <oskar.pala92@gmail.com>
2024-05-02 21:54:43 +0200

    created random numbers generator

--- src/main/java/LottoReciver/LottoReceiver.java
+++ src/main/java/LottoReciver/LottoReceiver.java
@@ -5,23 +5,38 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class LottoReceiver {
-    Set<Integer> numbersFromUser = new HashSet<>();
 
-    public Set<Integer> getNumbersFromUser(Scanner scanner) {
+
+    public Set<Integer> getNumbersFromUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<Integer> numbersFromUser = new HashSet<>();
 
 
         while (numbersFromUser.size() < 6) {
+
             int inputNumbersCounter = 6 - numbersFromUser.size();
             System.out.printf("Give %s numbers %n", inputNumbersCounter);
             while (!scanner.hasNextInt()) {
-                System.out.println("Cos");
+                System.out.println("Enter an integer in a range 1 and 99");
                 scanner.next();
             }
-            numbersFromUser.add(scanner.nextInt());
+            int givenNumber = scanner.nextInt();
+            validateInputNumber(givenNumber, numbersFromUser);
             System.out.println(numbersFromUser);
         }
+        scanner.close();
         return numbersFromUser;
     }
 
-    ;
+    private void validateInputNumber(int givenNumber, Set<Integer> numbersFromUser) {
+        if (givenNumber > 0 && givenNumber < 100) {
+            numbersFromUser.add(givenNumber);
+        } else {
+            System.out.printf("You provide %s, Please provide number beetwen 1 and 99 %n", givenNumber);
+        }
+
+
+    }
+
 }
